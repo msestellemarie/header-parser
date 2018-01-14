10356b734b11f7958cc4de250052d52f3a3b6e8b
Estelle Sillari-Owen <emso18@gmail.com>
2018-01-13 22:14:46 -0500

    cleaned up index.js

--- index.js
+++ index.js
@@ -12,5 +12,3 @@ app.get('/', function(req, res){
     }
   );
 });
-
-//{"host":"localhost:5000","connection":"keep-alive","cache-control":"max-age=0","user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36","upgrade-insecure-requests":"1","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","dnt":"1","accept-encoding":"gzip, deflate, br","accept-language":"en-US,en;q=0.9"}
